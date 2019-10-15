b4b75a9cac68e2b6da84bb35ae4bfade7f5d4e1c
Marco Centurión <mcenturion@protonmail.com>
2019-10-15 15:52:42 -0300

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,39 @@
+# @summary A short summary of the purpose of this defined type.
+#
+# A description of what this defined type does
+#
+# @example
+#   mhn_cowrie { 'namevar': }
+define mhn_cowrie (
+  Integer $port,
+  String $user,
+) {
+
+  ensure_packages(
+    [python-virtualenv, git, gcc],
+    {ensure => present},
+  )
+
+  vcsrepo {'/opt/cowrie':
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/micheloosterhof/cowrie.git',
+    revision => '34f8464',
+  }
+  
+  supervisor::program {'cowrie':
+    ensure    => present,
+    enable    => true,
+    command   => '/opt/cowrie/bin/cowrie start',
+    directory => '/opt/cowrie',
+    user      => $user,
+    require   => Vcsrepo['/opt/cowrie'],
+  }
+
+  python::virtualenv {'/opt/cowrie':
+    ensure   => present,
+    version  => '2.7',
+    venv_dir => 'cowrie-env',
+    require  => Vcsrepo['/opt/cowrie'],    
+  }
+}
---
+++ spec/defines/mhn_cowrie_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe 'mhn_cowrie' do
+  let(:title) { 'namevar' }
+  let(:params) do
+    {
+      'port' => 22,
+      'user' => 'cowrie',
+    }
+  end
+
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
+      it { is_expected.to contain_class('supervisor') }
+      it { is_expected.to contain_package('python-virtualenv') }
+      it { is_expected.to contain_package('git') }
+      it { is_expected.to contain_package('gcc') }
+      it {
+        is_expected.to contain_vcsrepo('/opt/cowrie').with(
+          'ensure' => 'present',
+          'provider' => 'git',
+          'source' => 'https://github.com/micheloosterhof/cowrie.git',
+          'revision' => '34f8464',
+        )
+      }
+    end
+  end
+end
